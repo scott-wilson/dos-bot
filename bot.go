0f0e7b548620b267f771b28714de6d7b62f112cd
Scott Wilson <scott@propersquid.com>
2017-12-05 21:58:33 -0800

    Added commands to let the bot send messages to the server.

--- bot.go
+++ bot.go
@@ -8,4 +8,9 @@ type Bot interface {
 	Name() string
 	ID() interface{}
 	DirectedMessageRegex() *regexp.Regexp
+	SendMessage(Room, string) error
+	SendDirectMessage(Room, User, string) error
+	SendEmote(Room, string) error
+	SendPrivateMessage(User, string) error
+	SendPrivateEmote(User, string) error
 }
